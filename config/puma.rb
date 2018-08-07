fdf19a72b837393909f3ea025c6fd6c709933462
chase-stevens <chasestevenspersonal@gmail.com>
2018-08-07 09:18:08 -0700

    Implement basic login

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     ||  300
+port        ENV['PORT']     ||  3000
 environment ENV['RACK_ENV'] ||  'development'
 
 on_worker_boot do
